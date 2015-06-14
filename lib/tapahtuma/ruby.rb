dab41f51ef665943fe1f864feab823ac761dcc83
elf <edipofederle@gmail.com>
2015-06-14 19:51:28 -0300

    Inicial Commit

---
+++ lib/tapahtuma/ruby.rb
@@ -0,0 +1,7 @@
+require "tapahtuma/ruby/version"
+
+module Tapahtuma
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/tapahtuma/ruby/version.rb
@@ -0,0 +1,5 @@
+module Tapahtuma
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
